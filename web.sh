4ff3e784f9fbec89e9852cb8b936e85d20e88f41
jdzlaoda2 <97279971+jdzlaoda2@users.noreply.github.com>
2023-10-10 14:26:07 +0800

    Update web.sh

--- web.sh
+++ web.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+UUID=${UUID:-'99276a60-67e6-4147-ebb6-f3f5494cec52'}
 VMESS_WSPATH=${VMESS_WSPATH:-'/'}
 VLESS_WSPATH=${VLESS_WSPATH:-'/'}
 TROJAN_WSPATH=${TROJAN_WSPATH:-'/'}
